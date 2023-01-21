296843524581c0408a3e0748d3cbbfa3ba66b56e
Marcin 🐈 Golenia <marcin.golenia@ignite.no>
2023-01-22 00:19:57 +0100

    FileCopier: Copying root ska

--- src/FileCopier.fs
+++ src/FileCopier.fs
@@ -1,4 +1,15 @@
 module FilesCopier
 
-let copy ska =
-    ()
+open System.IO
+
+let copyFiles fromPath toPath =
+    let files = Directory.GetFiles(fromPath)
+    toPath |> Directory.CreateDirectory
+    files
+    |> Array.iter(fun file ->
+        Directory.CreateDirectory(Path.GetDirectoryName(file))
+        let fileName = Path.GetFileName(file)
+        if fileName.StartsWith("ska_") then
+            ()
+        else
+            File.Copy(file, Path.Combine(toPath, Path.GetFileName(file))))
--- tests/FilesCopier.fs
+++ tests/FilesCopier.fs
@@ -1,11 +1,40 @@
 module SourceFilesCopier
 
+open System
+open System.IO
 open Xunit
+open FsUnit.Xunit
 
 [<Fact>]
-let ``All files of the ska directory and subdirectories should be copied`` () =
+let ``Ska: All files of the ska directory should be copied except ska definition`` () =
     // Arrange
-    let path = "skas"
+    let toPath = Path.Combine(Directory.GetCurrentDirectory(), Guid.NewGuid().ToString())
+    let fromPath = Path.Combine("skas", "go-backend")
     // Act
-    FilesCopier.copy ""
-    ()
+    FilesCopier.copyFiles fromPath toPath
+    // Assert
+    Directory.GetFiles toPath |> Array.map(fun file -> file |> Path.GetFileName) |> should equal  [|"papa.json"; "root.go"|]
+//     
+// [<Fact>]
+// let ``Ska: All subdirectories that don't start with "ska" including files should be copied`` () =
+//     // Arrange
+//     let path = Path.Combine("skas", "go-backend")
+//     // Act
+//     FilesCopier.copyDir path
+//     ()
+//     
+// [<Fact>]
+// let ``Ska option: All files in the directory should be copied`` () =
+//     // Arrange
+//     let path = Path.Combine("skas", "go-backend")
+//     // Act
+//     FilesCopier.copyDir path
+//     ()
+//     
+// [<Fact>]
+// let ``Ska option: All subdirectories including files should be copied`` () =
+//     // Arrange
+//     let path = Path.Combine("skas", "go-backend")
+//     // Act
+//     FilesCopier.copyDir path
+//     ()
