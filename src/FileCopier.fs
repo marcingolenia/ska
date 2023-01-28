fb898145d3b3f04e6261d30a64cc09e727f393b5
Marcin 🐈 Golenia <marcin.golenia@ignite.no>
2023-01-29 00:18:58 +0100

    💾 Files copying without ska_ stuff 💾

--- src/FileCopier.fs
+++ src/FileCopier.fs
@@ -3,13 +3,11 @@ module FilesCopier
 open System.IO
 
 let copyFiles fromPath toPath =
-    let files = Directory.GetFiles(fromPath)
     toPath |> Directory.CreateDirectory
-    files
-    |> Array.iter(fun file ->
-        Directory.CreateDirectory(Path.GetDirectoryName(file))
-        let fileName = Path.GetFileName(file)
-        if fileName.StartsWith("ska_") then
-            ()
-        else
-            File.Copy(file, Path.Combine(toPath, Path.GetFileName(file))))
+    Directory.GetFiles(fromPath, "*.*", SearchOption.AllDirectories)
+    |> Array.filter(fun path -> not <| path.Contains("ska_"))
+    |> Array.iter(fun filePath ->
+        let pathToBuild = Path.GetDirectoryName(filePath.Replace(fromPath + Path.DirectorySeparatorChar.ToString(), ""))
+        Path.Combine(toPath, pathToBuild) |> Directory.CreateDirectory
+        let targetPath = Path.Combine(toPath, pathToBuild, Path.GetFileName(filePath))
+        File.Copy(filePath, targetPath))
--- tests/FilesCopier.fs
+++ tests/FilesCopier.fs
@@ -8,21 +8,30 @@ open FsUnit.Xunit
 [<Fact>]
 let ``Ska: All files of the ska directory should be copied except ska definition`` () =
     // Arrange
-    let toPath = Path.Combine(Directory.GetCurrentDirectory(), Guid.NewGuid().ToString())
+    let toPath =
+        Path.Combine(Directory.GetCurrentDirectory(), Guid.NewGuid().ToString())
     let fromPath = Path.Combine("skas", "go-backend")
     // Act
     FilesCopier.copyFiles fromPath toPath
     // Assert
-    Directory.GetFiles toPath |> Array.map(fun file -> file |> Path.GetFileName) |> should equal  [|"papa.json"; "root.go"|]
-//     
-// [<Fact>]
-// let ``Ska: All subdirectories that don't start with "ska" including files should be copied`` () =
-//     // Arrange
-//     let path = Path.Combine("skas", "go-backend")
-//     // Act
-//     FilesCopier.copyDir path
-//     ()
-//     
+    Directory.GetFiles toPath
+    |> Array.map(fun file -> file |> Path.GetFileName)
+    |> should equal [| "papa.json"; "root.go" |]
+
+[<Fact>]
+let ``Ska: All subdirectories that don't start with "ska_" including files should be copied`` () =
+    // Arrange
+    let toPath =
+        Path.Combine(Directory.GetCurrentDirectory(), Guid.NewGuid().ToString())
+    let fromPath = Path.Combine("skas", "go-backend")
+    // Act
+    FilesCopier.copyFiles fromPath toPath
+    // Assert
+    let copiedFiles =
+        Directory.GetFiles(toPath, "*.*", SearchOption.AllDirectories)
+        |> Array.map(fun filePath -> filePath.Replace($"{toPath}{Path.DirectorySeparatorChar}", ""))
+    copiedFiles |> should equal [| "papa.json"; "root.go"; "subdir/whaterver.md" |]
+//
 // [<Fact>]
 // let ``Ska option: All files in the directory should be copied`` () =
 //     // Arrange
@@ -30,7 +39,7 @@ let ``Ska: All files of the ska directory should be copied except ska definition
 //     // Act
 //     FilesCopier.copyDir path
 //     ()
-//     
+//
 // [<Fact>]
 // let ``Ska option: All subdirectories including files should be copied`` () =
 //     // Arrange
