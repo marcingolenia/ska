7011fee61d579b328c95e4ad257bd07dba078d8a
Marcin 🐈 Golenia <marcin.golenia@ignite.no>
2023-03-05 00:00:07 +0100

    minor refactor

--- src/ContentTuner.fs
+++ src/ContentTuner.fs
@@ -4,19 +4,17 @@ open System.IO
 open System.Text
 
 let replaceVars (variables: Map<string, string>) (line:string): string =
-    let d = (Map.fold(fun (acc: string) key value -> acc.Replace($"skavar_{key}", value)) line variables)
-    d
+    (Map.fold(fun (acc: string) key value -> acc.Replace($"skavar_{key}", value)) line variables)
 
 let tuneSka (ska: Domain.Ska) (variables: Map<string, string>) (path: string) =
     let selectedSkaOptionIds =
         ska.Options |> List.map(fun opt -> Path.GetFileNameWithoutExtension opt.Path)
     let indexedLines = File.ReadAllLines path |> Array.indexed
-    let allSkasInFile =
+
+    let linesNumbersToRemove =
         indexedLines
         |> Array.filter(fun (_, line) -> line.Contains "+ska_" || line.Contains("-ska_"))
         |> Array.map(fun (lineNo, line) -> (lineNo, line[line.IndexOf("ska_")..]))
-    let linesNumbersToRemove =
-        allSkasInFile
         |> Array.filter(fun (_, ska) -> selectedSkaOptionIds |> List.contains ska |> not)
         |> Array.pairwise
         |> Array.mapi(fun i pair -> i % 2 = 0, pair)
--- src/FileCopier.fs
+++ src/FileCopier.fs
@@ -2,25 +2,26 @@ module FilesCopier
 
 open System.IO
 
+let createPathAndCopyFile (skaDirectory: string) (toPath: string) (fromPath: string) =
+    let pathToBuild =
+        Path.GetDirectoryName(fromPath.Replace(skaDirectory + Path.DirectorySeparatorChar.ToString(), ""))
+    Path.Combine(toPath, pathToBuild) |> Directory.CreateDirectory |> ignore
+    let targetPath = Path.Combine(toPath, pathToBuild, Path.GetFileName(fromPath))
+    File.Copy(fromPath, targetPath)
+
 let copyFiles (ska: Domain.Ska) toPath =
     toPath |> Directory.CreateDirectory |> ignore
-    let dirPath =
-        if File.GetAttributes ska.Path = FileAttributes.Directory then
-             ska.Path
-        else
-            Path.GetDirectoryName  ska.Path
-    let skaOptionDirPaths = ska.Options |> List.map(fun opt -> Path.GetDirectoryName opt.Path)
+    let skaDirectory = Path.GetDirectoryName ska.Path
+    let skaOptionDirPaths =
+        ska.Options |> List.map(fun opt -> Path.GetDirectoryName opt.Path)
     let allOptionsDirPaths =
-        Directory.GetFiles(dirPath, "ska_*.yaml", SearchOption.AllDirectories)
+        Directory.GetFiles(skaDirectory, "ska_*.yaml", SearchOption.AllDirectories)
         |> Array.filter(fun path -> path <> ska.Path)
         |> Array.map(Path.GetDirectoryName)
         |> Array.toList
     let notSelectedOptionsDirPaths = allOptionsDirPaths |> List.except skaOptionDirPaths
-    Directory.GetFiles(dirPath, "*.*", SearchOption.AllDirectories)
-        |> Array.filter(fun path -> (path.Contains "ska_" || notSelectedOptionsDirPaths |> List.exists path.Contains) |> not)
-        |> Array.iter(fun filePath ->
-            let pathToBuild =
-                Path.GetDirectoryName(filePath.Replace(dirPath + Path.DirectorySeparatorChar.ToString(), ""))
-            Path.Combine(toPath, pathToBuild) |> Directory.CreateDirectory |> ignore
-            let targetPath = Path.Combine(toPath, pathToBuild, Path.GetFileName(filePath))
-            File.Copy(filePath, targetPath))
+    Directory.GetFiles(skaDirectory, "*.*", SearchOption.AllDirectories)
+    |> Array.filter(fun path ->
+        (path.Contains "ska_" || notSelectedOptionsDirPaths |> List.exists path.Contains)
+        |> not)
+    |> Array.iter(createPathAndCopyFile skaDirectory toPath)
